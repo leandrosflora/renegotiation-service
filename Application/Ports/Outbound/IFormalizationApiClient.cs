4febc11e3ceda88f5e6465446b0ca13423266124
Leandro Flora <leandrosflora@gmail.com>
2026-07-18 01:15:32 -0300

    propagate confirmation idempotency key

--- Application/Ports/Outbound/IFormalizationApiClient.cs
+++ Application/Ports/Outbound/IFormalizationApiClient.cs
@@ -4,7 +4,10 @@ namespace renegotiation_service.Application.Ports.Outbound;
 
 public interface IFormalizationApiClient
 {
-    Task<AgreementConfirmationResult> ConfirmAsync(string simulationId, CancellationToken cancellationToken);
+    Task<AgreementConfirmationResult> ConfirmAsync(
+        string simulationId,
+        string idempotencyKey,
+        CancellationToken cancellationToken);
 
     Task<DocumentResult> GetDocumentAsync(string agreementId, CancellationToken cancellationToken);
 }
